f2b1b0b6a011c4d7e1046274c66a2f8376ea0ca3
mr.mojtaba <mr.mojtaba47@gmail.com>
2025-10-28 16:51:59 +0330

    Add Coil dependencies, new vector asset, and update color resources; refactor MainActivity to use a grid layout

--- app/src/main/java/com/mes71/compose_ui/MainActivity.kt
+++ app/src/main/java/com/mes71/compose_ui/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.mes71.compose_ui.ui.theme.Compose_UiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,78 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Compose_UiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainApp()
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainApp() {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(), topBar = {
+            TopAppBar(
+                colors = androidx.compose.material3.TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(R.color.primary_500),
+                    titleContentColor = colorResource(R.color.white)
+                ),
+                title = { Text(text = "Compose UI") },
+
+                )
+        }) { innerPadding ->
+
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+
+        ) {
+            item {
+                gridItem()
+            }
+
+
+        }
+
+
+    }
+
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    Compose_UiTheme {
-        Greeting("Android")
+fun gridItem() {
+
+    Card(
+        modifier = Modifier.height(180.dp).fillMaxWidth()
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Box(
+                modifier = Modifier.weight(3f).fillMaxWidth()
+            ) {
+
+            }
+            Text("Online Groceries",  modifier = Modifier.padding(bottom = 8.dp))
+        }
+
+
     }
+
 }
+
+
+
+
+
+
+
